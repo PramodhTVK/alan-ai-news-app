c8400c5d8e38768388edfa6f82130871da057f55
PramodhTVK <pramodhkannan1829@gmail.com>
2023-04-28 22:40:04 +0530

    first commit

--- src/components/NewsData.js
+++ src/components/NewsData.js
@@ -1,17 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import { getNews } from "../Service/getNews";
 
 export default function NewsData(){
-    
-        const getAllNews = () =>{
-            getNews()
+    const [newsData,setNewsData] = useState([])
+        const getAllNews = async () =>{
+           setNewsData(await getNews())
         }
     useEffect(()=>{
         getAllNews()
     },[])
+    console.log(newsData?.data?.articles)
     return (
         <div className="main">
             <h1>Voice News</h1>
+            {newsData.map((news) => {
+                return(
+                    <div>{}</div>
+                )
+            })}
         </div>
     )
 }
